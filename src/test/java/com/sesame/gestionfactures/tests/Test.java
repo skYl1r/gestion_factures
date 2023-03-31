ec595062eb6e771bc3c27982c848f8d71034c73b
hjandoubi <jendoubi564@gmail.com>
2023-03-31 18:39:49 +0100

    init project

---
+++ src/test/java/com/sesame/gestionfactures/tests/Test.java
@@ -0,0 +1,10 @@
+package com.sesame.gestionfactures.tests;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String res = new StringBuilder().append((Object)null).toString();
+		System.out.println(res );
+	}
+
+}
